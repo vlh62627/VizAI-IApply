597a9c5fc2a060a69b65917907621e9dfa366a4a
vlh62627 <vlh62627@gmail.com>
2026-03-11 03:00:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -58,8 +58,8 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # 3. Main title and subtitle using HTML for proper size
-st.markdown('<h1 style="text-align:center; font-size:50px; color:white; margin-bottom:10px;">✈️ CareerPilot AI</h1>', unsafe_allow_html=True)
-st.markdown('<h3 style="text-align:center; font-size:28px; color:white; margin-bottom:40px;">Your AI assistant that finds jobs and applies automatically</h3>', unsafe_allow_html=True)
+st.markdown('<h1 style="text-align:left; font-size:40px; color:white; margin-bottom:10px;">✈️ CareerPilot AI</h1>', unsafe_allow_html=True)
+st.markdown('<h3 style="text-align:left; font-size:10px; color:white; margin-bottom:20px;">Your AI assistant that finds jobs and applies automatically</h3>', unsafe_allow_html=True)
 
 # 4. Inputs on main page inside a card
 st.markdown('<div class="card">', unsafe_allow_html=True)
