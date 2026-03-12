be750f2f458739e2b7783babfb0b3e6dd2a18d17
vlh62627 <vlh62627@gmail.com>
2026-03-11 22:19:47 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+EMAIL_USER = os.getenv("EMAIL_USER")
+EMAIL_PASS = os.getenv("EMAIL_PASS")
